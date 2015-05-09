c9e156bf8a3f7e244d5d8aa8369051e118138f19
Nils Werner <nils.werner@gmail.com>
2015-05-09 12:20:35 +0200

    Show merge status at the end

--- gitgrid/pages/branches.py
+++ gitgrid/pages/branches.py
@@ -24,13 +24,13 @@ class Branches(Page):
             else:
                 self.controller.lights[i, center] = utils.string_to_rgb(fn)[0]
                 if git.get_can_merge_ff(fn):
-                    self.controller.lights[i, center+1] = numpy.array([0, 1, 0])
+                    self.controller.lights[i, -1] = numpy.array([0, 1, 0])
                 elif git.get_can_merge(fn):
-                    self.controller.lights[i, center+1] = numpy.array([1, 1, 0])
+                    self.controller.lights[i, -1] = numpy.array([1, 1, 0])
                 else:
-                    self.controller.lights[i, center+1] = numpy.array([1, 0, 0])
+                    self.controller.lights[i, -1] = numpy.array([1, 0, 0])
             self.controller.lights[i, start:center] = numpy.array([1, 0, 0])
-            self.controller.lights[i, center+2:end+2] = numpy.array([0, 1, 0])
+            self.controller.lights[i, center+1:end+1] = numpy.array([0, 1, 0])
 
         def new_branch(*args):
             if not git.do_new_branch():
