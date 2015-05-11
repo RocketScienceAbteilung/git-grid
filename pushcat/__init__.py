c38c6d73089a51144899161b19356808ceb4d725
Nils Werner <nils.werner@audiolabs-erlangen.de>
2015-05-11 11:53:00 +0200

    Some more pushcat comments, as some of the code is a bit weird

--- pushcat/__init__.py
+++ pushcat/__init__.py
@@ -8,20 +8,22 @@ import collections
 
 
 class PushCat(object):  # Like netcat, but for push
-
-
     def callback(self, message):
+        # pitchweel touch: start scrolling
         if message.type == 'pitchwheel' and message.pitch != 0:
             pitch = (message.pitch + 2. ** 13) / 2. ** 14
             # prevent out of bounds when scrolling all the way up
             self.scrollwheel = min(self.buflen - 4, int(self.buflen * pitch))
             self.redraw()
+        # pitchweel release: scroll all the way down again
         elif message.type == 'pitchwheel' and message.pitch == 0:
             self.scrollwheel = 0
             self.redraw()
 
     def redraw(self):
         self.lock.acquire()
+
+        # Get 4 lines of text, according to current scrollwheel position
         for i, bufline in enumerate(
             itertools.islice(
                 self.lines,
@@ -29,9 +31,14 @@ class PushCat(object):  # Like netcat, but for push
                 self.buflen - self.scrollwheel
             )
         ):
+            # - Remove newline, it prints an a-umlaut on the controller
+            # - Add spaces to fill 68 chars because Push only prints stuff if
+            #   the sysex cmd is actually long enough
             bufline = bufline[:-1].ljust(68)
+            # - Truncate if line is too long
             bufline = (bufline[:66] + '..') if len(bufline) > 68 else bufline
 
+            # - Map to sysex command
             data = [71, 127, 21, 24+i, 0, 69, 0] + map(ord, bufline)
 
             self.output.send(
@@ -46,10 +53,12 @@ class PushCat(object):  # Like netcat, but for push
         self.lock = threading.Lock()
 
         try:
+            # Ports on MAC OSX
             self.output = mido.open_output('Ableton Push User Port')
             self.input = mido.open_input('Ableton Push User Port',
                                          callback=self.callback)
         except IOError:
+            # Ports on Arch Linux
             self.output = mido.open_output('Ableton Push MIDI 2')
             self.input = mido.open_input('Ableton Push MIDI 2',
                                          callback=self.callback)
@@ -57,8 +66,12 @@ class PushCat(object):  # Like netcat, but for push
         while True:
             line = sys.stdin.readline()
             if not line:
+                # EOF from STDIN
                 break
 
+            # We need to use mutex because concurrent reads and writes on
+            # dequeue (scrolling while there is new text coming in from stdin)
+            # will issue warnings.
             self.lock.acquire()
             self.lines.append(line)
             self.lines.popleft()
